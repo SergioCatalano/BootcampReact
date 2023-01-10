d092cdf195e943dfb3e0213d32121b75fa36caa2
Sergio Catalano <shcatalano53@gmail.com>
2023-01-10 18:51:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Mensaje from './Mensaje.js'
 
 function App() {
+
+  const hola = "hola mmuymendo";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {hola}
+      <Mensaje color='red' message='Estamos trabajndo '/>
+      <Mensaje color='green' message='En un curso'/>
+      <Mensaje color='yellow' message='de REact'/>
     </div>
   );
 }
